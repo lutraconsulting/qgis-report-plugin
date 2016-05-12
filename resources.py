2863d7012bfd8875a040775ae5da3284f7311c68
Peter Petrik <zilolv@gmail.com>
2016-05-12 08:45:21 +0200

    fix metadata

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed May 11 10:33:50 2016
+# Created: Wed May 11 16:24:04 2016
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
